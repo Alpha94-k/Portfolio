c0b3f8e2d0ef663f3b31de7ae2ce6577da029285
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2022-05-24 16:47:23 +0200

    maj

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,52 +37,54 @@ fetch('./assets/data/projects.json')
 
 function createCard(project){
     const divProjects = document.querySelector('.projects-content')
-    var article = document.createElement('article')
-    article.classList.add('project-card')
-    var img = document.createElement('img')
-    img.classList.add('projects-img')
-    img.src= `./assets/img/${project.picture}`
-    img.alt = project.alt
-    article.append(img)
-    var overlay = document.createElement('div')
-    overlay.classList.add('project-overlay')
-    article.append(overlay)
-    var modal = document.createElement('div')
-    modal.classList.add('project-modal')
-    var divContent = document.createElement('div')
-
-    var subtitle = document.createElement('span')
-    subtitle.classList.add('project-subtitle')
-    subtitle.textContent = project.subtitle
-    divContent.append(subtitle)
-
-    var title = document.createElement('h3')
-    title.classList.add('project-title')
-    title.textContent= project.title
-    divContent.append(title)
-
-    var txtDescri = document.createElement('p')
-    txtDescri.classList.add('project-content')
-    txtDescri.textContent = project.description
-    divContent.append(txtDescri)
-
-    var button = document.createElement('a')
-    button.href = project.url
-    button.alt = project.alt
-    button.rel = 'nofollow'
-    button.target = '_blank'
-    button.classList.add('project-button')
-    button.classList.add('button')
-
-    var icone = document.createElement('i')
-    icone.classList.add('fa')
-    icone.classList.add('fa-link')
-    icone.ariaHidden = true
-
-    button.append(icone)
-    divContent.append(button)
-
-    modal.append(divContent)
-    article.append(modal)
-    divProjects.append(article)
+    if(project.state == true){
+        var article = document.createElement('article')
+        article.classList.add('project-card')
+        var img = document.createElement('img')
+        img.classList.add('projects-img')
+        img.src= `./assets/img/${project.picture}`
+        img.alt = project.alt
+        article.append(img)
+        var overlay = document.createElement('div')
+        overlay.classList.add('project-overlay')
+        article.append(overlay)
+        var modal = document.createElement('div')
+        modal.classList.add('project-modal')
+        var divContent = document.createElement('div')
+    
+        var subtitle = document.createElement('span')
+        subtitle.classList.add('project-subtitle')
+        subtitle.textContent = project.subtitle
+        divContent.append(subtitle)
+    
+        var title = document.createElement('h3')
+        title.classList.add('project-title')
+        title.textContent= project.title
+        divContent.append(title)
+    
+        var txtDescri = document.createElement('p')
+        txtDescri.classList.add('project-content')
+        txtDescri.textContent = project.description
+        divContent.append(txtDescri)
+    
+        var button = document.createElement('a')
+        button.href = project.url
+        button.alt = project.alt
+        button.rel = 'nofollow'
+        button.target = '_blank'
+        button.classList.add('project-button')
+        button.classList.add('button')
+    
+        var icone = document.createElement('i')
+        icone.classList.add('fa')
+        icone.classList.add('fa-link')
+        icone.ariaHidden = true
+    
+        button.append(icone)
+        divContent.append(button)
+    
+        modal.append(divContent)
+        article.append(modal)
+        divProjects.append(article)
+    }
 }
